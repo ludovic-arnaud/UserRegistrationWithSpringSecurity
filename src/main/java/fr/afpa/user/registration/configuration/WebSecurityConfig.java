113b803639783a6fec04fadd3d2aad9ca40b96b6
Ludovic Arnaud <ludovic.arnaud@live.fr>
2020-06-26 10:50:41 +0200

    add Javadoc

--- src/main/java/fr/afpa/user/registration/configuration/WebSecurityConfig.java
+++ src/main/java/fr/afpa/user/registration/configuration/WebSecurityConfig.java
@@ -13,78 +13,73 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 // TODO: Auto-generated Javadoc
 /**
- * The Class WebSecurityConfig.
- * Spring Security Java Configuration, fichier de configuration
- * Crée le Servlet Filter springSecurityFilterChain responsable de la sécurité (protection des url, validatation des login, mot de passe...)
+ * The Class WebSecurityConfig. Spring Security Java Configuration, fichier de
+ * configuration Crée le Servlet Filter springSecurityFilterChain responsable de
+ * la sécurité (protection des url, validatation des login, mot de passe...)
  * Gère l'authentification à notre place.
  * 
  */
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-	
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
 	/** The user details service. */
 	@Autowired
-    private UserDetailsService userDetailsService;
+	private UserDetailsService userDetailsService;
 
-    /**
-     * B crypt password encoder.
-     *
-     * @return the b crypt password encoder
-     */
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	/**
+	 * B crypt password encoder.
+	 *
+	 * @return the b crypt password encoder
+	 */
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    /**
-     * La méthode configure définit quelles url sont sécurisées et lesquelles non.
-     * Ici, les pages /registration et /login ne nécessitent pas d'identification
-     * Les autres url qui existent comme /welcome nécessitent elles d'être identifiées
-     * Si un utilisateur non identifié tente d'y accéder, il sera renvoyé sur /login
-     * 
-     * On peut créer des accès par Role !!!
-     *
-     * @param http the http
-     * @throws Exception the exception
-     */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/resources/**", "/registration").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
-    }
+	/**
+	 * La méthode configure définit quelles url sont sécurisées et lesquelles non.
+	 * Ici, les pages /registration et /login ne nécessitent pas d'identification
+	 * Les autres url qui existent comme /welcome nécessitent elles d'être
+	 * identifiées Si un utilisateur non identifié tente d'y accéder, il sera
+	 * renvoyé sur /login
+	 * 
+	 * On peut créer des accès par Role !!!
+	 * 
+	 *
+	 * @param http the http
+	 * @throws Exception the exception
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/resources/**", "/registration").permitAll().anyRequest().authenticated()
+				.and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+	}
 
-    /**
-     * Custom authentication manager.
-     * L'AuthenticationManager possède la méthode authenticate qui permet d'authentifier l'user en utilisant l'objet Authentication
-     * Cet objet contient les informations de l'utilisateur et notemment ses granted authorities.
-     *
-     * @return the authentication manager
-     * @throws Exception the exception
-     */
-    @Bean
-    public AuthenticationManager customAuthenticationManager() throws Exception {
-        return authenticationManager();
-    }
+	/**
+	 * Custom authentication manager. L'AuthenticationManager possède la méthode
+	 * authenticate qui permet d'authentifier l'user en utilisant l'objet
+	 * Authentication Cet objet contient les informations de l'utilisateur et
+	 * notemment ses granted authorities.
+	 *
+	 * @return the authentication manager
+	 * @throws Exception the exception
+	 */
+	@Bean
+	public AuthenticationManager customAuthenticationManager() throws Exception {
+		return authenticationManager();
+	}
 
-    /**
-     * On met en place une authentification avec le service userDetailsService + encodage du mot de passe
-     *
-     * @param auth the auth
-     * @throws Exception the exception
-     */
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-    }
+	/**
+	 * On met en place une authentification avec le service userDetailsService +
+	 * encodage du mot de passe
+	 *
+	 * @param auth the auth
+	 * @throws Exception the exception
+	 */
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
 
 }
